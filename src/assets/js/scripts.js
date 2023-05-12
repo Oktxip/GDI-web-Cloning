71c187e8499a9be2a891b6b6752bd945fc5f2d8f
Oktxip <Oktaiwp849@gmail.com>
2023-05-12 10:59:41 +0700

    carousel

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -7,9 +7,6 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-// Import jQuery
-import * as $ from 'jquery'
-
 $(document).ready(function () {
   console.log('Welcome');
 
@@ -20,7 +17,7 @@ $(document).ready(function () {
 
 // navbar scrolled ---------------------------------------------------------
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var header = document.getElementById('header');
   const scrollPosition = window.scrollY || window.pageYOffset;
   if (window.scrollY > 0) {
@@ -43,85 +40,91 @@ hamburgerIcon.addEventListener('click', () => {
 
 
 // feature switch -----------------------------------------------------------------
-  function showContent(contentId) {
-    // Hide all content
-    const contents = document.getElementsByClassName("js-ctn");
-    for (let i = 0; i < contents.length; i++) {
-      contents[i].classList.remove("on");
-    }
-
-    // Show selected content
-    const content = document.getElementById(contentId);
-    content.classList.add("on");
-
-    // Set on button
-    const buttons = document.getElementsByTagName("button");
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].classList.remove("on");
-    }
-    
-    
-    const onButton = document.querySelector(`.show-content-button[data-content="${contentId}"]`);
-    onButton.classList.add("on");
+function showContent(contentId) {
+  // Hide all content
+  const contents = document.getElementsByClassName("js-ctn");
+  for (let i = 0; i < contents.length; i++) {
+    contents[i].classList.remove("on");
   }
+
+  // Show selected content
+  const content = document.getElementById(contentId);
+  content.classList.add("on");
+
+  // Set on button
+  const buttons = document.getElementsByTagName("button");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("on");
+  }
+
+
+  const onButton = document.querySelector(`.show-content-button[data-content="${contentId}"]`);
+  onButton.classList.add("on");
+}
 
 // slider cs --------------------------------------------------------------------
 
-const carousel = document.querySelector('.carousel');
-const carouselContainer = document.querySelector('.carousel-container');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const carouselBullets = document.querySelector('.carousel-bullets');
+// const carousel = document.querySelector('.carousel');
+// const carouselContainer = document.querySelector('.carousel-container');
+// const carouselItems = document.querySelectorAll('.carousel-item');
+// const carouselBullets = document.querySelector('.carousel-bullets');
 
-// Initialize variables
-let currentSlide = 0;
-let intervalId;
+// // Initialize variables
+// let currentSlide = 0;
+// let intervalId;
 
-// Create bullets
-carouselItems.forEach((item, index) => {
-  const bullet = document.createElement('button');
-  bullet.classList.add('bullet');
-  if (index === 0) {
-    bullet.classList.add('active');
-  }
-  bullet.addEventListener('click', () => {
-    clearInterval(intervalId);
-    goToSlide(index);
-  });
-  carouselBullets.appendChild(bullet);
-});
+// // Create bullets
+// carouselItems.forEach((item, index) => {
+//   const bullet = document.createElement('button');
+//   bullet.classList.add('bullet');
+//   if (index === 0) {
+//     bullet.classList.add('active');
+//   }
+//   bullet.addEventListener('click', () => {
+//     clearInterval(intervalId);
+//     goToSlide(index);
+//   });
+//   carouselBullets.appendChild(bullet);
+// });
 
-// Go to a specific slide
-function goToSlide(slideIndex) {
-  carouselItems[currentSlide].classList.remove('active');
-  carouselBullets.childNodes[currentSlide].classList.remove('active');
-  carouselItems[slideIndex].classList.add('active');
-  carouselBullets.childNodes[slideIndex].classList.add('active');
-  currentSlide = slideIndex;
-}
+// // Go to a specific slide
+// function goToSlide(slideIndex) {
+//   carouselItems[currentSlide].classList.remove('active');
+//   carouselBullets.childNodes[currentSlide].classList.remove('active');
+//   carouselItems[slideIndex].classList.add('active');
+//   carouselBullets.childNodes[slideIndex].classList.add('active');
+//   currentSlide = slideIndex;
+// }
 
-// Next slide
-function nextSlide() {
-  clearInterval(intervalId);
-  const nextSlideIndex = currentSlide + 1 >= carouselItems.length ? 0 : currentSlide + 1;
-  goToSlide(nextSlideIndex);
-}
+// // Next slide
+// function nextSlide() {
+//   clearInterval(intervalId);
+//   const nextSlideIndex = currentSlide + 1 >= carouselItems.length ? 0 : currentSlide + 1;
+//   goToSlide(nextSlideIndex);
+// }
 
-// Previous slide
-function prevSlide() {
-  clearInterval(intervalId);
-  const prevSlideIndex = currentSlide - 1 < 0 ? carouselItems.length - 1 : currentSlide - 1;
-  goToSlide(prevSlideIndex);
-}
+// // Previous slide
+// function prevSlide() {
+//   clearInterval(intervalId);
+//   const prevSlideIndex = currentSlide - 1 < 0 ? carouselItems.length - 1 : currentSlide - 1;
+//   goToSlide(prevSlideIndex);
+// }
 
-// Set interval for auto slide
-intervalId = setInterval(() => {
-  nextSlide();
-}, 5000);
+// // Set interval for auto slide
+// intervalId = setInterval(() => {
+//   nextSlide();
+// }, 5000);
 
-// Event listeners for arrow buttons
-document.querySelector('.carousel-prev').addEventListener('click', prevSlide);
-document.querySelector('.carousel-next').addEventListener('click', nextSlide);
+// // Event listeners for arrow buttons
+// document.querySelector('.carousel-prev').addEventListener('click', prevSlide);
+// document.querySelector('.carousel-next').addEventListener('click', nextSlide);
 
 // slick slider -------------------------------------------------------------
+$(document).ready(function () {
+  $('.carousel').slick({
+    dots: true,
+    infinite: false
+  });
+});
 // sidebar slider ----------------------------------------------------------
 
