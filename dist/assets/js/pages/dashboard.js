d32d13e684261908d0a3cb1aebb5610a5911e6f2
Oktxip <Oktaiwp849@gmail.com>
2023-05-25 06:54:46 +0700

    new update

---
+++ dist/assets/js/pages/dashboard.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************************!*\
+  !*** ./src/assets/js/pages/dashboard.js ***!
+  \******************************************/
+$(document).ready(function () {
+console.log('okta tampan');
+
+  
+})
+
+
+/******/ })()
+;
+//# sourceMappingURL=dashboard.js.map
