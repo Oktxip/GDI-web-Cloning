2b6f2153efd32d2e6f69178596a01b245d93fb0d
Oktxip <Oktaiwp849@gmail.com>
2023-05-25 09:46:25 +0700

    new

--- dist/assets/js/pages/dashboard.js
+++ dist/assets/js/pages/dashboard.js
@@ -4,9 +4,17 @@ var __webpack_exports__ = {};
   !*** ./src/assets/js/pages/dashboard.js ***!
   \******************************************/
 $(document).ready(function () {
-console.log('okta tampan');
-
   
+  function toggleSidebar() {
+    var sidebarHeaders = document.querySelectorAll('.js-sidebar-check');
+  
+    sidebarHeaders.forEach(function(sidebarHeader) {
+      sidebarHeader.classList.toggle('closed');
+    });
+  }
+
+  var button = document.getElementById('btn-sidebar-toggle')
+  button.addEventListener("click", toggleSidebar)
 })
 
 
--- src/assets/js/pages/dashboard.js
+++ src/assets/js/pages/dashboard.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-console.log('okta tampan');
-
   
+  function toggleSidebar() {
+    var sidebarHeaders = document.querySelectorAll('.js-sidebar-check');
+  
+    sidebarHeaders.forEach(function(sidebarHeader) {
+      sidebarHeader.classList.toggle('closed');
+    });
+  }
+
+  var button = document.getElementById('btn-sidebar-toggle')
+  button.addEventListener("click", toggleSidebar)
 })
 
