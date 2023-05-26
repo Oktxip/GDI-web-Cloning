a938bbb64c408fdc60acabcb65209d41d24bcc21
Oktxip <Oktaiwp849@gmail.com>
2023-05-26 08:46:49 +0700

    new dashboard

--- dist/assets/js/pages/dashboard.js
+++ dist/assets/js/pages/dashboard.js
@@ -7,9 +7,15 @@ $(document).ready(function () {
   
   function toggleSidebar() {
     var sidebarHeaders = document.querySelectorAll('.js-sidebar-check');
-  
+    
     sidebarHeaders.forEach(function(sidebarHeader) {
       sidebarHeader.classList.toggle('closed');
+    });
+    
+    var mainDashboards = document.querySelectorAll('.js-dashboard-check');
+
+    mainDashboards.forEach(function(mainDashboard) {
+      mainDashboard.classList.toggle('close-main-dashboard');
     });
   }
 
